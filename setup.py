01ef1f7481ba4c1f719f7861b0692f6fc057b83f
keishimizu26629 <keisukeshimizu.inoworl@gmail.com>
2024-09-05 11:16:32 +0900

    fix: bug in setup.py

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ includes = [
     "reportlab",
     "tkinter",
     "PIL",
+    "traceback",
 ]
 
 excludes = [
@@ -66,13 +67,13 @@ build_exe_options = {
     "includes": includes,  # 必要なパッケージをここに追加
     "excludes": excludes,  # 除外するパッケージをここに追加
     "include_files": [
-        icon,
+        (icon, "icons/icon.ico"),
         "resource.res"
     ],
 }
 
 setup(name = 'renamePdf',
-    version = '3.0',
+    version = '4.0',
     description = 'A dedicated application for processing PDFs generated when ordering system baths through the PU Order System. This application reads site information from the text within PDFs and renames the files based on this information. It also calculates the deadline for finalizing the shipping date based on the shipping information and records it within the PDF. Additionally, it performs other PDF manipulations to streamline workflow processes.',
     options={"build_exe": build_exe_options},
     executables = executables
